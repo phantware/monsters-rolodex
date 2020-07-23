7fceb120fb1d4729084acb1ff7955fc88765a599
phantware <phantmoney2011@gmail.com>
2020-07-23 14:28:00 +0100

    Updates

---
+++ precache-manifest.f82dda2ac41979bf1bbeff01febbb9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c0f0ca012232d0cfa978328834a8a7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "92d1231b6281bf135934",
+    "url": "/monsters-rolodex/static/css/main.187fe3f9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d1231b6281bf135934",
+    "url": "/monsters-rolodex/static/js/main.8e965adb.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
